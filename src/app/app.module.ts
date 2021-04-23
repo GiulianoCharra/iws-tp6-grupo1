61bf0c1c080b1ec3ce9fce9264df650ea2cc57ba
GiulianoCharra <cnchongonator@gmail.com>
2021-04-23 00:09:52 -0300

    intentando hacer esto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,27 @@ import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { CheckoutComponent } from "./componentes/checkout/checkout.component";
-import { ProductosComponent } from "./componentes/productos/productos.component";
-import { CategoriasComponent } from "./componentes/categorias/categorias.component";
 import {
   NgbPaginationModule,
   NgbModalModule
 } from "@ng-bootstrap/ng-bootstrap";
+import { EfectivoComponent } from './componentes/formaPago/efectivo/efectivo.component';
+import { TarjetaVISAComponent } from './componentes/formaPago/tarjeta-visa/tarjeta-visa.component';
+import { PedidoComponent } from './componentes/pedido/pedido.component';
+import { MenuBarComponent } from './componentes/menu-bar/menu-bar.component';
+import { RouterModule } from "@angular/router";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, NgbPaginationModule, NgbModalModule],
+  imports: [BrowserModule, FormsModule, NgbPaginationModule, NgbModalModule,RouterModule.forRoot([
+      { path: '', component: PedidoComponent },
+    ])],
   declarations: [
     AppComponent,
-    CategoriasComponent,
-    ProductosComponent,
-    CheckoutComponent
+    CheckoutComponent,
+    EfectivoComponent,
+    TarjetaVISAComponent,
+    PedidoComponent,
+    MenuBarComponent
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/componentes/formaPago/tarjeta-visa/tarjeta-visa.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tarjeta-visa',
+  templateUrl: './tarjeta-visa.component.html',
+  styleUrls: ['./tarjeta-visa.component.css']
+})
+export class TarjetaVISAComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
